46cdc755eb18414787f16954211620521bd7582d
narek <narek@DESKTOP-98JS365>
2024-06-19 19:25:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    static void Main()
+    {
+        string testString = "Hello, world!";
+        Console.WriteLine($"Обратная строка: {ReverseString(testString)}");
+    }
+}
+
